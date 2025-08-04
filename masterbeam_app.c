0083de991132223e95b4c2ee72c09d88b594a73e
Amphyby <amphyby@protonmail.com>
2025-08-04 23:46:44 +0200

    continuation

--- masterbeam_app.c
+++ masterbeam_app.c
@@ -1,1 +1,27 @@
+#include "masterbeam_app.h"
 
+MasterbeamApp* allocate() {
+    MasterbeamApp* app = malloc(sizeof(MasterbeamApp));
+
+    app->gui = furi_record_open(RECORD_GUI);
+
+    app->bt = furi_record_open(RECORD_BT);
+
+    app->view_dispatcher = view_dispatcher_alloc();
+    view_dispatcher_enable_queue(app->view_dispatcher);
+    view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
+    view_dispatcher_set_custom_event_callback(app->view_dispatcher, hid_custom_event_callback);
+    view_dispatcher_set_navigation_event_callback(app->view_dispatcher, hid_back_event_callback);
+
+    return app;
+}
+
+int32_t main_masterbeam(void* p) {
+    UNUSED(p);
+
+    MasterbeamApp* app = allocate();
+
+    bt_disconnect(app->bt);
+
+    return 0;
+}
--- masterbeam_app.h
+++ masterbeam_app.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <furi.h>
+#include <furi_hal_bt.h>
+#include <bt/bt_service/bt.h>
+#include <gui/gui.h>
+#include <gui/view.h>
+#include <gui/view_dispatcher.h>
+#include <gui/scene_manager.h>
+#include <notification/notification.h>
+
 #include "scenes/masterbeam_scene.h"
 
 #define HID_BT_KEYS_STORAGE_NAME ".bt_masterbeam.keys"
@@ -8,7 +17,6 @@ typedef struct MasterbeamApp MasterbeamApp;
 struct MasterbeamApp {
     SceneManager* scene_manager;
     ViewDispatcher* view_dispatcher;
-    Submenu* submenu;
     Widget* widget;
     FuriHalBleProfileBase* ble_hid_profile;
     Bt* bt;
